f8f7315175ffc6a32124f1d4c2f7af2435699707
IsacHolstad <wisac22@gmail.com>
2023-06-06 11:31:36 +0200

    skills section made

---
+++ src/components/Skills.js
@@ -0,0 +1,104 @@
+import React from 'react';
+
+function Skills () {
+    return (
+        <>
+            <div className="bg-gray-900 py-24 sm:py-32">
+                <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                    <h2 className="text-center text-lg font-semibold leading-8 text-white">
+                        Skills
+                    </h2>
+                    <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-8">
+                        <img
+                            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png"
+                            alt="Transistor"
+                            width={158}
+                            height={48}
+                        />
+                        <img
+                            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/2048px-HTML5_logo_and_wordmark.svg.png"
+                            alt="Reform"
+                            width={158}
+                            height={48}
+                        />
+                        <img
+                            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                            src="https://cdn.iconscout.com/icon/free/png-512/css3-8-1175200.png"
+                            alt="Tuple"
+                            width={158}
+                            height={48}
+                        />
+                        <img
+                            className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
+                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"
+                            alt="SavvyCal"
+                            width={158}
+                            height={48}
+                        />
+                        <img
+                            className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+                            src="https://vitejs.dev/logo.svg"
+                            alt="Statamic"
+                            width={158}
+                            height={48}
+                        />
+                        <img
+                            className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+                            src="https://cdn.sanity.io/images/599r6htc/localized/46a76c802176eb17b04e12108de7e7e0f3736dc6-1024x1024.png?w=670&h=670&q=75&fit=max&auto=format"
+                            alt="Statamic"
+                            width={158}
+                            height={48}
+                        />
+                        <img
+                            className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+                            src="https://img.uxwing.com/wp-content/themes/uxwing/download/brands-social-media/postman-icon.png"
+                            alt="Statamic"
+                            width={158}
+                            height={48}
+                        />
+                        <img
+                            className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+                            src="https://seeklogo.com/images/S/supabase-logo-DCC676FFE2-seeklogo.com.png"
+                            alt="Statamic"
+                            width={158}
+                            height={48}
+                        />
+                        <img
+                            className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/2048px-Tailwind_CSS_Logo.svg.png"
+                            alt="Statamic"
+                            width={158}
+                            height={48}
+                        />
+                        <img
+                            className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+                            src="https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png"
+                            alt="Statamic"
+                            width={158}
+                            height={48}
+                        />
+                        <img
+                            className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/WebStorm_Icon.svg/1200px-WebStorm_Icon.svg.png"
+                            alt="Statamic"
+                            width={158}
+                            height={48}
+                        />
+                        <img
+                            className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+                            src="https://cdn.icon-icons.com/icons2/3053/PNG/512/sourcetree_macos_bigsur_icon_189711.png"
+                            alt="Statamic"
+                            width={158}
+                            height={48}
+                        />
+                    </div>
+                </div>
+            </div>
+
+        </>
+    );
+};
+
+export default Skills;
